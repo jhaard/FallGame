b88d0f258d862000fbcf6ca17d52424b3eabb347
jhaard <jorgen.hard@gmail.com>
2024-03-22 09:52:26 +0100

    Created an abstract DynamicBody-class for movable objects. Also a Player-class for the player.

---
+++ app/src/main/java/com/example/fallgame/Player.kt
@@ -0,0 +1,38 @@
+package com.example.fallgame
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Rect
+
+class Player(context: Context, posX: Float, posY: Float, size: Float, speed: Float, vGravity: Float):
+    DynamicBody(context, posX, posY, size, speed, vGravity) {
+
+    private var bitmap: Bitmap
+    private val sizable = BitmapFactory.Options()
+
+
+    init {
+        sizable.inSampleSize = 14
+        bitmap = BitmapFactory.decodeResource(
+            context.resources,
+            R.drawable.simple_icon_rockfall_400)
+    }
+    override fun bounds(bounds: Rect) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun draw(canvas: Canvas?) {
+        val centerX = posX - bitmap.width / 2
+        val centerY = posY - bitmap.height / 2
+
+        canvas?.drawBitmap(bitmap, centerX, centerY, null)
+    }
+
+    override fun update() {
+        //TODO("Not yet implemented")
+    }
+
+
+}
