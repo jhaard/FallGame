a737355cc647fcd3ed2a6e64015b880659cdc7e8
jhaard <jorgen.hard@gmail.com>
2024-03-27 15:38:38 +0100

    New background.

--- app/src/main/java/com/example/fallgame/Background.kt
+++ app/src/main/java/com/example/fallgame/Background.kt
@@ -19,12 +19,12 @@ class Background(context: Context, posX: Float, posY: Float, size: Float, speed:
         sizable.inSampleSize = 10
         bitmap = BitmapFactory.decodeResource(
             context.resources,
-            R.drawable.new_test_background
+            R.drawable.new_test_background1
         )
         bitmapList = listOf(
-            BitmapFactory.decodeResource(context.resources, R.drawable.new_test_background),
-            BitmapFactory.decodeResource(context.resources, R.drawable.new_test_background),
-            BitmapFactory.decodeResource(context.resources, R.drawable.new_test_background))
+            BitmapFactory.decodeResource(context.resources, R.drawable.new_test_background1),
+            BitmapFactory.decodeResource(context.resources, R.drawable.new_test_background1),
+            BitmapFactory.decodeResource(context.resources, R.drawable.new_test_background1))
     }
     override fun bounds(bounds: Rect) {
     }
