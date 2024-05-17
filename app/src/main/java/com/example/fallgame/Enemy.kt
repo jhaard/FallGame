4cf6bcfc3d86f717c8e9e20c977538977dc09a9a
jhaard <jorgen.hard@gmail.com>
2024-05-17 12:03:46 +0200

    Enemy-class added.

---
+++ app/src/main/java/com/example/fallgame/Enemy.kt
@@ -0,0 +1,25 @@
+package com.example.fallgame
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Rect
+
+class Enemy(context: Context, bitmap: Bitmap, posX: Float, posY: Float, vGravity: Float):
+    DynamicBody(context, bitmap,  posX, posY, vGravity) {
+    override fun bounds(bounds: Rect) {
+
+    }
+
+    override fun draw(canvas: Canvas?) {
+        val centerX = posX - bitmap.width / 2
+        val centerY = posY - bitmap.height / 2
+
+        canvas?.drawBitmap(bitmap, centerX, centerY, null)
+    }
+
+    override fun update() {
+
+    }
+
+}
