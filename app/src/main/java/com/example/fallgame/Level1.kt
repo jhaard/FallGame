7190d14fc99457e3225b385dd224994255d5171c
jhaard <jorgen.hard@gmail.com>
2024-05-17 12:09:10 +0200

    Draw paper-plane enemy on screen.

--- app/src/main/java/com/example/fallgame/Level1.kt
+++ app/src/main/java/com/example/fallgame/Level1.kt
@@ -7,11 +7,14 @@ import android.view.SurfaceHolder
 
 class Level1(private val context: Context) : Scene() {
     override lateinit var player: Player
+    override lateinit var enemy: Enemy
     override lateinit var background: Background
     private val playerSize = BitmapFactory.Options()
+    private val enemySize = BitmapFactory.Options()
 
     init {
         playerSize.inSampleSize = 1
+        enemySize.inSampleSize = 1
     }
 
     override fun start(holder: SurfaceHolder) {
@@ -20,6 +23,12 @@ class Level1(private val context: Context) : Scene() {
             BitmapFactory.decodeResource(context.resources, R.drawable.test_1_rockfall_player_400, playerSize),
             holder.surfaceFrame.width().toFloat() / 2,
             500f, 0f
+        )
+        enemy = Enemy(
+            context,
+            BitmapFactory.decodeResource(context.resources, R.drawable.enemy_paperplane_right_400, enemySize),
+            holder.surfaceFrame.width().toFloat() / 2,
+            800f, 0f
         )
         background = Background(
             context, BitmapFactory.decodeResource(
@@ -33,6 +42,7 @@ class Level1(private val context: Context) : Scene() {
         background.draw(canvas)
         background.update()
         player.draw(canvas)
+        enemy.draw(canvas)
         holder.unlockCanvasAndPost(canvas)
     }
 
